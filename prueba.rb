1935424a972a94d0a5f88e53a6f6d314192fd4cc
Dani <dustyglass@gmail.com>
2018-07-23 15:58:26 -0400

    FIX: indentation

--- prueba.rb
+++ prueba.rb
@@ -4,37 +4,37 @@ file.close
 
 def averages(readfile)
   file = File.open('promedios.csv', 'w')
-    readfile.each do |student|
+  readfile.each do |student|
     studentinfo = student.split(', ').map(&:chomp)
     name = studentinfo.shift
     sum = studentinfo.inject(0) { |suma, grade| suma + grade.to_f }
     amount = studentinfo.length
     avr = sum / amount
     file.puts "El promedio de #{name} es #{avr}"
-    end
+  end
   file.close
 end
 
 def absences(readfile)
   totalabsent = 0
-    readfile.each do |student|
+  readfile.each do |student|
     studentinfo = student.split(', ').map(&:chomp)
     studentinfo.shift
     absent = studentinfo.count('A')
     totalabsent += absent
-    end
+  end
   puts "El total de inasistencias es #{totalabsent}"
 end
 
 def fails(readfile)
   readfile.each do |student|
-  studentinfo = student.split(', ').map(&:chomp)
-  name = studentinfo.shift
-  sum = studentinfo.inject(0) { |suma, grade| suma + grade.to_f }
-  amount = studentinfo.length
-  avr = sum / amount
-  puts "#{name}: aprobado" if avr > 5
-  puts "#{name}: reprobado" if avr < 5
+    studentinfo = student.split(', ').map(&:chomp)
+    name = studentinfo.shift
+    sum = studentinfo.inject(0) { |suma, grade| suma + grade.to_f }
+    amount = studentinfo.length
+    avr = sum / amount
+    puts "#{name}: aprobado" if avr > 5
+    puts "#{name}: reprobado" if avr < 5
   end
 end
 # Menu start
@@ -49,16 +49,16 @@ while option != 4
   p 'Opción 4: Salir'
 
   option = gets.chomp.to_i
-puts case option
-     when 1
-       averages(data)
-     when 2
-       absences(data)
-     when 3
-       fails(data)
-     when 4
-       exit
-     else
-       p 'Esa no es una opción válida. Recuerda ingresar un número del 1 al 4'
-     end
+  puts case option
+       when 1
+         averages(data)
+       when 2
+         absences(data)
+       when 3
+         fails(data)
+       when 4
+         exit
+       else
+         p 'Esa no es una opción válida. Recuerda ingresar un número del 1 al 4'
+       end
 end
